36a465d28216ba5938b263e85ca77f98d8ab6aa3
David Foster <david.foster@privva.com>
2021-11-18 21:11:03 -0600

    add datasets

--- migration.rb
+++ migration.rb
@@ -18,16 +18,22 @@ dev_client = Aws::QuickSight::Client.new(
 )
 
 
-# list data sources
+# data sources
 DEV_DATA_SOURCES = dev_client.list_data_sources({ aws_account_id: DEV_AWS_ACCOUNT_ID })
 
+#DEV_DATA_SOURCES[:data_sources].each do |source|
+  #puts "#{source[:name]}"
+  #puts "#{source[:arn]}"
+  #puts "#{source[:data_source_id]}"
+  #puts "\n"
+#end
 
+# data sets
+DEV_DATA_SETS = dev_client.list_data_sets({ aws_account_id: DEV_AWS_ACCOUNT_ID })
 
-DEV_DATA_SOURCES[:data_sources].each do |source|
-  puts "#{source[:name]}"
-  puts "#{source[:arn]}"
-  puts "#{source[:data_source_id]}"
+DEV_DATA_SETS[:data_set_summaries].each do |summary|
+  puts "#{summary[:name]}"
+  puts "#{summary[:arn]}"
+  puts "#{summary[:data_set_id]}"
   puts "\n"
 end
-
-
