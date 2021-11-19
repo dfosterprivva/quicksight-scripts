83e961bef34a5ccf916c3341203dd545b7ba3239
David Foster <david.foster@privva.com>
2021-11-19 13:12:35 -0600

    working source creation

--- create_data_source.rb
+++ create_data_source.rb
@@ -1,225 +1,68 @@
 #!/usr/bin/env ruby
 
+require 'aws-sdk-quicksight'
 require './variables.rb'
 
-resp = client.create_data_source({
-  aws_account_id: TARGET_AWS_ACCOUNT_ID, # required
-  data_source_id: "ResourceId", # required
-  name: "ResourceName", # required
-  type: "ADOBE_ANALYTICS", # required, accepts ADOBE_ANALYTICS, AMAZON_ELASTICSEARCH, ATHENA, AURORA, AURORA_POSTGRESQL, AWS_IOT_ANALYTICS, GITHUB, JIRA, MARIADB, MYSQL, ORACLE, POSTGRESQL, PRESTO, REDSHIFT, S3, SALESFORCE, SERVICENOW, SNOWFLAKE, SPARK, SQLSERVER, TERADATA, TWITTER, TIMESTREAM, AMAZON_OPENSEARCH
+#scoped params
+DATA_SOURCE_ID=''
+RESOURCE_NAME=''
+TYPE='POSTGRESQL'
+
+
+# initiate connection to target
+@target_client = Aws::QuickSight::Client.new(
+  region: AWS_REGION,
+  access_key_id: TARGET_AWS_ACCESS_KEY_ID,
+  secret_access_key: TARGET_AWS_SECRET_ACCESS_KEY
+)
+
+resp = @target_client.create_data_source({
+  aws_account_id: TARGET_AWS_ACCOUNT_ID,
+  data_source_id: DATA_SOURCE_ID,
+  name: RESOURCE_NAME,
+  type: TYPE,
   data_source_parameters: {
-    amazon_elasticsearch_parameters: {
-      domain: "Domain", # required
-    },
-    athena_parameters: {
-      work_group: "WorkGroup",
-    },
-    aurora_parameters: {
-      host: "Host", # required
-      port: 1, # required
-      database: "Database", # required
-    },
-    aurora_postgre_sql_parameters: {
-      host: "Host", # required
-      port: 1, # required
-      database: "Database", # required
-    },
-    aws_iot_analytics_parameters: {
-      data_set_name: "DataSetName", # required
-    },
-    jira_parameters: {
-      site_base_url: "SiteBaseUrl", # required
-    },
-    maria_db_parameters: {
-      host: "Host", # required
-      port: 1, # required
-      database: "Database", # required
-    },
-    my_sql_parameters: {
-      host: "Host", # required
-      port: 1, # required
-      database: "Database", # required
-    },
-    oracle_parameters: {
-      host: "Host", # required
-      port: 1, # required
-      database: "Database", # required
-    },
-    postgre_sql_parameters: {
-      host: "Host", # required
-      port: 1, # required
-      database: "Database", # required
-    },
-    presto_parameters: {
-      host: "Host", # required
-      port: 1, # required
-      catalog: "Catalog", # required
-    },
+    #postgre_sql_parameters: {
+      #host: TARGET_RDS_HOSTNAME,
+      #port: TARGET_RDS_PORT,
+      #database: TARGET_RDS_DB_NAME,
+    #},
     rds_parameters: {
-      instance_id: "InstanceId", # required
-      database: "Database", # required
-    },
-    redshift_parameters: {
-      host: "Host",
-      port: 1,
-      database: "Database", # required
-      cluster_id: "ClusterId",
-    },
-    s3_parameters: {
-      manifest_file_location: { # required
-        bucket: "S3Bucket", # required
-        key: "S3Key", # required
-      },
-    },
-    service_now_parameters: {
-      site_base_url: "SiteBaseUrl", # required
-    },
-    snowflake_parameters: {
-      host: "Host", # required
-      database: "Database", # required
-      warehouse: "Warehouse", # required
-    },
-    spark_parameters: {
-      host: "Host", # required
-      port: 1, # required
-    },
-    sql_server_parameters: {
-      host: "Host", # required
-      port: 1, # required
-      database: "Database", # required
-    },
-    teradata_parameters: {
-      host: "Host", # required
-      port: 1, # required
-      database: "Database", # required
-    },
-    twitter_parameters: {
-      query: "Query", # required
-      max_rows: 1, # required
-    },
-    amazon_open_search_parameters: {
-      domain: "Domain", # required
+      instance_id: TARGET_RDS_INSTANCE_ID,
+      database: TARGET_RDS_DB_NAME,
     },
   },
   credentials: {
     credential_pair: {
-      username: "Username", # required
-      password: "Password", # required
-      alternate_data_source_parameters: [
-        {
-          amazon_elasticsearch_parameters: {
-            domain: "Domain", # required
-          },
-          athena_parameters: {
-            work_group: "WorkGroup",
-          },
-          aurora_parameters: {
-            host: "Host", # required
-            port: 1, # required
-            database: "Database", # required
-          },
-          aurora_postgre_sql_parameters: {
-            host: "Host", # required
-            port: 1, # required
-            database: "Database", # required
-          },
-          aws_iot_analytics_parameters: {
-            data_set_name: "DataSetName", # required
-          },
-          jira_parameters: {
-            site_base_url: "SiteBaseUrl", # required
-          },
-          maria_db_parameters: {
-            host: "Host", # required
-            port: 1, # required
-            database: "Database", # required
-          },
-          my_sql_parameters: {
-            host: "Host", # required
-            port: 1, # required
-            database: "Database", # required
-          },
-          oracle_parameters: {
-            host: "Host", # required
-            port: 1, # required
-            database: "Database", # required
-          },
-          postgre_sql_parameters: {
-            host: "Host", # required
-            port: 1, # required
-            database: "Database", # required
-          },
-          presto_parameters: {
-            host: "Host", # required
-            port: 1, # required
-            catalog: "Catalog", # required
-          },
-          rds_parameters: {
-            instance_id: "InstanceId", # required
-            database: "Database", # required
-          },
-          redshift_parameters: {
-            host: "Host",
-            port: 1,
-            database: "Database", # required
-            cluster_id: "ClusterId",
-          },
-          s3_parameters: {
-            manifest_file_location: { # required
-              bucket: "S3Bucket", # required
-              key: "S3Key", # required
-            },
-          },
-          service_now_parameters: {
-            site_base_url: "SiteBaseUrl", # required
-          },
-          snowflake_parameters: {
-            host: "Host", # required
-            database: "Database", # required
-            warehouse: "Warehouse", # required
-          },
-          spark_parameters: {
-            host: "Host", # required
-            port: 1, # required
-          },
-          sql_server_parameters: {
-            host: "Host", # required
-            port: 1, # required
-            database: "Database", # required
-          },
-          teradata_parameters: {
-            host: "Host", # required
-            port: 1, # required
-            database: "Database", # required
-          },
-          twitter_parameters: {
-            query: "Query", # required
-            max_rows: 1, # required
-          },
-          amazon_open_search_parameters: {
-            domain: "Domain", # required
-          },
-        },
-      ],
+      username: TARGET_RDS_USER,
+      password: TARGET_RDS_PWD,
     },
-    copy_source_arn: "CopySourceArn",
+    # don't think we need this
+#    copy_source_arn: "CopySourceArn",
   },
   permissions: [
     {
-      principal: "Principal", # required
-      actions: ["String"], # required
+      principal: TARGET_PRINCIPAL_USER_ARN,
+      actions: [
+        "quicksight:UpdateDataSourcePermissions",
+        "quicksight:DescribeDataSource",
+        "quicksight:DescribeDataSourcePermissions",
+        "quicksight:PassDataSource",
+        "quicksight:UpdateDataSource",
+        "quicksight:DeleteDataSource"
+      ]
     },
   ],
   vpc_connection_properties: {
-    vpc_connection_arn: "Arn", # required
+    vpc_connection_arn: TARGET_VPC_ARN,
   },
   ssl_properties: {
     disable_ssl: false,
   },
   tags: [
     {
-      key: "TagKey", # required
-      value: "TagValue", # required
+      key: "Name", # required
+      value: RESOURCE_NAME, # required
     },
   ],
 })
--- migration.rb
+++ migration.rb
@@ -3,8 +3,8 @@ require 'aws-sdk-quicksight'
 
 require './variables.rb'
 
-# initiate connection to dev
-@dev_client = Aws::QuickSight::Client.new(
+# initiate connection to source
+@source_client = Aws::QuickSight::Client.new(
   region: AWS_REGION,
   access_key_id: SOURCE_AWS_ACCESS_KEY_ID,
   secret_access_key: SOURCE_AWS_SECRET_ACCESS_KEY
@@ -13,9 +13,9 @@ require './variables.rb'
 
 # data sources
 def get_data_sources(source_account_id)
-  dev_data_sources = @dev_client.list_data_sources({ aws_account_id: source_account_id })
+  source_data_sources = @source_client.list_data_sources({ aws_account_id: source_account_id })
 
-  dev_data_sources[:data_sources].each do |source|
+  source_data_sources[:data_sources].each do |source|
     puts "#{source[:name]}"
     puts "#{source[:arn]}"
     puts "#{source[:data_source_id]}"
@@ -26,15 +26,15 @@ end
 
 # data sets
 def get_data_sets(source_account_id)
-  dev_data_sets = @dev_client.list_data_sets({ aws_account_id: source_account_id})
+  source_data_sets = @source_client.list_data_sets({ aws_account_id: source_account_id})
 
-  dev_data_sets[:data_set_summaries].each do |summary|
+  source_data_sets[:data_set_summaries].each do |summary|
     puts "#{summary[:name]}"
-    puts "#{summary[:arn]}"
     puts "#{summary[:data_set_id]}"
+    #puts "#{summary[:arn]}"
     puts "\n"
   end
 end
 
-#get_data_sources(SOURCE_AWS_ACCOUNT_ID)
+get_data_sources(SOURCE_AWS_ACCOUNT_ID)
 #get_data_sets(SOURCE_AWS_ACCOUNT_ID)
