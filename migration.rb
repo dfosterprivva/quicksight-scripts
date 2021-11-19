81ee6ef009c85a5afb6622737ea01d64bae0ad30
David Foster <david.foster@privva.com>
2021-11-19 12:10:06 -0600

    change to methods

--- migration.rb
+++ migration.rb
@@ -11,7 +11,7 @@ SANDBOX_AWS_SECRET_ACCESS_KEY = ENV['SANDBOX_AWS_SECRET_ACCESS_KEY']
 SANDBOX_AWS_ACCOUNT_ID = ENV['SANDBOX_AWS_ACCOUNT_ID']
 
 # initiate connection to dev
-dev_client = Aws::QuickSight::Client.new(
+@dev_client = Aws::QuickSight::Client.new(
   region: 'us-east-1',
   access_key_id: DEV_AWS_ACCESS_KEY_ID,
   secret_access_key: DEV_AWS_SECRET_ACCESS_KEY
@@ -19,21 +19,29 @@ dev_client = Aws::QuickSight::Client.new(
 
 
 # data sources
-DEV_DATA_SOURCES = dev_client.list_data_sources({ aws_account_id: DEV_AWS_ACCOUNT_ID })
+def get_data_sources(source_account_id)
+  dev_data_sources = @dev_client.list_data_sources({ aws_account_id: source_account_id })
 
-#DEV_DATA_SOURCES[:data_sources].each do |source|
-  #puts "#{source[:name]}"
-  #puts "#{source[:arn]}"
-  #puts "#{source[:data_source_id]}"
-  #puts "\n"
-#end
+  dev_data_sources[:data_sources].each do |source|
+    puts "#{source[:name]}"
+    puts "#{source[:arn]}"
+    puts "#{source[:data_source_id]}"
+    puts "\n"
+  end
+end
+
 
 # data sets
-DEV_DATA_SETS = dev_client.list_data_sets({ aws_account_id: DEV_AWS_ACCOUNT_ID })
+def get_data_sets(source_account_id)
+  dev_data_sets = @dev_client.list_data_sets({ aws_account_id: source_account_id})
 
-DEV_DATA_SETS[:data_set_summaries].each do |summary|
-  puts "#{summary[:name]}"
-  puts "#{summary[:arn]}"
-  puts "#{summary[:data_set_id]}"
-  puts "\n"
+  dev_data_sets[:data_set_summaries].each do |summary|
+    puts "#{summary[:name]}"
+    puts "#{summary[:arn]}"
+    puts "#{summary[:data_set_id]}"
+    puts "\n"
+  end
 end
+
+#get_data_sources(DEV_AWS_ACCOUNT_ID)
+#get_data_sets(DEV_AWS_ACCOUNT_ID)
