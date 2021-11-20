b969057edd9230b06ac3fdb536c6ca6ac77448bf
David Foster <david.foster@privva.com>
2021-11-19 18:06:07 -0600

    stuck still

--- migration.rb
+++ migration.rb
@@ -42,24 +42,26 @@ def get_data_sets(source_account_id)
     dataset_details.data_set.arn = "arn:aws:quicksight:us-east-1:#{TARGET_AWS_ACCOUNT_ID}:dataset/#{data_set_id}"
     physical_table_id = dataset_details.data_set.physical_table_map.keys
     puts physical_table_id
-    puts "#{dataset_details.data_set.physical_table_map["#{physical_table_id}"].s3_source.data_source_arn.gsub("#{SOURCE_AWS_ACCOUNT_ID}","#{TARGET_AWS_ACCOUNT_ID}")}"
-    #target_data_source_arn = dataset_details.data_set.physical_table_map[physical_table_id].s3_source.data_source_arn.gsub("#{SOURCE_AWS_ACCOUNT_ID}","#{TARGET_AWS_ACCOUNT_ID}")
-    #puts target_data_source_arn
-    #dataset_details.data_set.physical_table_map["s3PhysicalTable"].s3_source.data_source_arn = target_data_source_arn
-    #puts "...Writing to file"
-    #File.open("#{data_set_name}.json", 'w') do |f|
-      #f.puts JSON.pretty_generate(dataset_details.data_set.to_h)
-    #end
+    if physical_table_id.include?("s3PhysicalTable")
+    target_data_source_arn = dataset_details.data_set.physical_table_map["\"#{physical_table_id}\""].s3_source.data_source_arn.gsub("#{SOURCE_AWS_ACCOUNT_ID}","#{TARGET_AWS_ACCOUNT_ID}")
+    dataset_details.data_set.physical_table_map["#{physical_table_id}"].s3_source.data_source_arn = target_data_source_arn
+    else
+    target_data_source_arn = dataset_details.data_set.physical_table_map["#{physical_table_id}"].relational_table.data_source_arn.gsub("#{SOURCE_AWS_ACCOUNT_ID}","#{TARGET_AWS_ACCOUNT_ID}")
+    dataset_details.data_set.physical_table_map["#{physical_table_id}"].relational_table.data_source_arn = target_data_source_arn
+    end
+    File.open("#{data_set_name}.json", 'w') do |f|
+      f.puts JSON.pretty_generate(dataset_details.data_set.to_h)
+    end
     puts "\n"
   end
 end
 
-#get_data_sets(SOURCE_AWS_ACCOUNT_ID)
+get_data_sets(SOURCE_AWS_ACCOUNT_ID)
 
-resp = @source_client.describe_data_set({
-  aws_account_id: SOURCE_AWS_ACCOUNT_ID,
-  data_set_id: "0a38e48a-d3fe-478f-a1d6-b17e6b1c63a5"
-  #data_set_id: "02d069aa-e68d-437b-8f38-1b8a76b2b39b"
-})
-binding.pry
+#resp = @source_client.describe_data_set({
+  #aws_account_id: SOURCE_AWS_ACCOUNT_ID,
+  #data_set_id: "0a38e48a-d3fe-478f-a1d6-b17e6b1c63a5"
+  ##data_set_id: "02d069aa-e68d-437b-8f38-1b8a76b2b39b"
+#})
+#binding.pry
 
