63da58fa2c1cf4c0cc2a2ec3c809c64d0debd403
David Foster <david.foster@privva.com>
2021-11-23 15:43:41 -0600

    rename

--- qs_data_source.rb
+++ qs_data_source.rb
@@ -20,7 +20,75 @@ require './variables.rb'
 # data sources
 
 def update_data_source(source)
-  puts "future update code here"
+  puts "UPDATING #{source.name} with ID: #{source.data_source_id}"
+  puts "\n"
+
+  #build out data_source_parameters
+  if source.data_source_parameters.rds_parameters != nil
+    source.data_source_parameters.rds_parameters[:instance_id] = TARGET_RDS_INSTANCE_ID
+    source.data_source_parameters.rds_parameters[:database] = TARGET_RDS_DB_NAME
+    credential_pair_hash = { credential_pair: {username: TARGET_RDS_USER, password: TARGET_RDS_PWD }}
+  else
+    abort("Parameter set not built yet")
+=begin
+Possible params to build
+amazon_elasticsearch_parameters
+athena_parameters
+aurora_parameters
+aurora_postgre_sql_parameters
+aws_iot_analytics_parameters
+jira_parameters
+maria_db_parameters
+my_sql_parameters
+oracle_parameters
+postgre_sql_parameters
+presto_parameters
+redshift_parameters
+s3_parameters
+service_now_parameters
+snowflake_parameters
+spark_parameters
+sql_server_parameters
+teradata_parameters
+twitter_parameters
+amazon_open_search_parameters
+=end
+  end
+
+  #create data source
+  resp = @target_client.update_data_source({
+    aws_account_id: TARGET_AWS_ACCOUNT_ID,
+    data_source_id: source.data_source_id,
+    name: source.name,
+    type: source.type,
+    data_source_parameters: source.data_source_parameters,
+    credentials: credential_pair_hash,
+    permissions: [
+      {
+        principal: TARGET_PRINCIPAL_USER_ARN,
+        actions: [
+          "quicksight:UpdateDataSourcePermissions",
+          "quicksight:DescribeDataSource",
+          "quicksight:DescribeDataSourcePermissions",
+          "quicksight:PassDataSource",
+          "quicksight:UpdateDataSource",
+          "quicksight:DeleteDataSource"
+        ]
+      },
+    ],
+    vpc_connection_properties: {
+      vpc_connection_arn: TARGET_VPC_ARN,
+    },
+    ssl_properties: {
+      disable_ssl: false,
+    },
+    tags: [
+      {
+        key: "Name", # required
+        value: source.name, # required
+      },
+    ],
+  })
 end
 
 def migrate_data_source(source)
@@ -115,7 +183,7 @@ def check_data_source
       then
       puts "Data source already Exists... updating"
       puts "\n"
-      # update method here
+      update_data_source(source)
     else
       puts "Data source does NOT exist... will migrate source"
       puts "\n"
